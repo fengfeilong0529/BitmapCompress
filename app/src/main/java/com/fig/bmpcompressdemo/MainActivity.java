dad64bb144df58cc5fedb737a23a1a6006d6f634
fengfeilong <fengfeilong>
2019-08-02 16:33:43 +0800

    调用系统的图片裁剪功能

--- app/src/main/java/com/fig/bmpcompressdemo/MainActivity.java
+++ app/src/main/java/com/fig/bmpcompressdemo/MainActivity.java
@@ -1,14 +1,19 @@
 package com.fig.bmpcompressdemo;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
@@ -26,6 +31,9 @@ import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int CAMERA_REQUEST_CODE = 11;
+    private static final int REQUEST_CUT_PHOTO = 12;
+    private static final int ALBUM_REQUEST_CODE = 13;
     @BindView(R.id.ivBeforeCompress)
     ImageView mIvBeforeCompress;
     @BindView(R.id.ivAfterCompress)
@@ -60,6 +68,13 @@ public class MainActivity extends AppCompatActivity {
     EditText mEtQualityLimit;
     @BindView(R.id.btnQualityLimitCompress)
     Button mBtnQualityLimitCompress;
+    @BindView(R.id.ivCropedImg)
+    ImageView mIvCropedImg;
+
+    private File cameraSavePath;//拍照照片路径
+    private Uri mUri;
+    private String photoName = System.currentTimeMillis() + ".jpg";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,10 +88,12 @@ public class MainActivity extends AppCompatActivity {
             getWindow().setAttributes(attributes);
         }
         bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.animal2);
+        cameraSavePath = new File(Environment.getExternalStorageDirectory().getPath() + "/" + photoName);
     }
 
 
-    @OnClick({R.id.btnQualityCompress, R.id.btnSampleSizeCompress, R.id.btnMatrixScaleCompress, R.id.btnRGB565Compress, R.id.btnCreateScaledBitmapCompress, R.id.btnQualityLimitCompress})
+    @OnClick({R.id.btnQualityCompress, R.id.btnSampleSizeCompress, R.id.btnMatrixScaleCompress, R.id.btnRGB565Compress,
+            R.id.btnCreateScaledBitmapCompress, R.id.btnQualityLimitCompress, R.id.btnCropAlbum, R.id.btnCropCamera})
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.btnQualityCompress:
@@ -96,6 +113,12 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.btnQualityLimitCompress:
                 compressBmp2LimitSize();
+                break;
+            case R.id.btnCropAlbum:
+                takePhotoByAlbum();
+                break;
+            case R.id.btnCropCamera:
+                takePhotoByCamera();
                 break;
         }
     }
@@ -195,6 +218,92 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
+    /****************************************************************************************************************/
+
+
+    /**
+     * 从相册获取照片
+     */
+    public void takePhotoByAlbum() {
+        checkReadStoragePermission();
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setDataAndType(MediaStore.Images.Media.INTERNAL_CONTENT_URI, "image/*");
+        startActivityForResult(intent, ALBUM_REQUEST_CODE);
+    }
+
+    /**
+     * 拍照获取照片
+     */
+    private void takePhotoByCamera() {
+        checkCameraPermission();
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            mUri = FileProvider.getUriForFile(MainActivity.this, "com.example.hxd.pictest.fileprovider", cameraSavePath);
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        } else {
+            mUri = Uri.fromFile(cameraSavePath);
+        }
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, mUri);
+        MainActivity.this.startActivityForResult(intent, CAMERA_REQUEST_CODE);
+    }
+
+    /**
+     * 裁剪照片
+     *
+     * @param uri
+     */
+    public void cropImg(Uri uri) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        intent.putExtra("crop", true);
+        // 裁剪框的比例，1：1
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        // 裁剪后输出图片的尺寸大小
+        intent.putExtra("outputX", 250);
+        intent.putExtra("outputY", 250);
+
+        intent.putExtra("outputFormat", "JPEG");// 图片格式
+        intent.putExtra("noFaceDetection", true);// 取消人脸识别
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, REQUEST_CUT_PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            if (requestCode == ALBUM_REQUEST_CODE) {
+                try {
+                    cropImg(data.getData());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (requestCode == CAMERA_REQUEST_CODE) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    cropImg(Uri.fromFile(cameraSavePath));
+                } else {
+                    cropImg(mUri);
+                }
+            } else if (requestCode == REQUEST_CUT_PHOTO) {
+                try {
+                    Bundle bundle = data.getExtras();
+                    if (bundle != null) {
+                        //在这里获得了剪裁后的Bitmap对象，可以用于上传
+                        Bitmap image = bundle.getParcelable("data");
+                        mIvCropedImg.setImageBitmap(image);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /****************************************************************************************************************/
+
+
     private void checkWriteStoragePermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
@@ -203,6 +312,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private void checkReadStoragePermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 0);
+            }
+        }
+    }
+
+    private void checkCameraPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 0);
+            }
+        }
+    }
 
     public void showTip(String tip) {
         mTvTips.setText(tip);
